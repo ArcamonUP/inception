15275231d732c49e591b2d0fbe90e2960fee6efb
ArcamonUP <kilfen@outlook.fr>
2025-06-19 14:46:35 +0200

    fix

--- srcs/requirements/wordpress/conf/config.sh
+++ srcs/requirements/wordpress/conf/config.sh
@@ -3,33 +3,32 @@ set -euo pipefail
 
 mkdir -p /var/www/wordpress
 cd /var/www/wordpress
-
-if [ -f wp-config.php ]; then
-    exec /usr/sbin/php-fpm7.4 -F
-fi
+rm -rf *
 
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar && mv wp-cli.phar /usr/local/bin/wp
 
 wp core download --allow-root
-wp config create --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_PASSWORD" --dbhost=mariadb --allow-root
+
+wp config create --dbname="$DB_NAME" --dbuser="$DB_USER" \
+                 --dbpass="$DB_PASSWORD" --dbhost=mariadb --allow-root
 
 for i in {1..20}; do
-    wp db check --allow-root >/dev/null 2>&1 && break
+    if wp db check --allow-root > /dev/null 2>&1; then
+        break
+    fi
     sleep 1
 done
 
-for i in {1..3}; do
-    wp core install --url="https://$DOMAIN_NAME" \
+wp core install --url="https://$DOMAIN_NAME" \
                     --title="$WP_TITLE" \
                     --admin_user="$WP_ADMIN_USR" \
                     --admin_password="$WP_ADMIN_PWD" \
                     --admin_email="$WP_ADMIN_EMAIL" \
-                    --skip-email --allow-root && break
-    sleep 10
-done
+                    --skip-email --allow-root
 
 wp user create "$WP_USER" "$WP_EMAIL" --role=author --user_pass="$WP_PWD" --allow-root
+
 wp theme install astra --activate --allow-root
 wp plugin install classic-editor --activate --allow-root
 wp plugin update --all --allow-root
@@ -38,7 +37,8 @@ chown -R www-data:www-data /var/www/wordpress
 find /var/www/wordpress -type d -exec chmod 755 {} \;
 find /var/www/wordpress -type f -exec chmod 644 {} \;
 
-sed -i 's|listen = /run/php/php7.4-fpm.sock|listen = 9000|' /etc/php/7.4/fpm/pool.d/www.conf
+sed -i 's|listen = /run/php/php7.4-fpm.sock|listen = 9000|' \
+       /etc/php/7.4/fpm/pool.d/www.conf
 mkdir -p /run/php
 
 exec /usr/sbin/php-fpm7.4 -F
