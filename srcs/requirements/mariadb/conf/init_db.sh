c126ab7cf8d24ca5ed42754bc218209083facd02
ArcamonUP <kilfen@outlook.fr>
2025-06-19 14:18:46 +0200

    fix: redem

--- srcs/requirements/mariadb/conf/init_db.sh
+++ srcs/requirements/mariadb/conf/init_db.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
 
-set -eo pipefail
-
-rm -f /var/lib/mysql/aria_log_control /var/lib/mysql/ibdata1.lock
-rm -f /var/run/mysqld/mysqld.pid || true
+DATA_DIR=/var/lib/mysql
+FLAG_FILE=$DATA_DIR/.initialized
 
 chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
-chmod -R 755 /var/lib/mysql
 
-if [ ! -d "/var/lib/mysql/mysql" ]; then
-    mariadbd --initialize-insecure --user=mysql --datadir=/var/lib/mysql
+if [ ! -d "$DATA_DIR/mysql" ]; then
+    mariadbd --initialize-insecure --user=mysql --datadir=$DATA_DIR
 fi
 
-mariadbd --user=mysql --bootstrap <<EOF
-FLUSH PRIVILEGES;
+if [ ! -f "$FLAG_FILE" ]; then
+    mariadbd --user=mysql --bootstrap <<EOF
 CREATE DATABASE IF NOT EXISTS $DB_NAME;
 CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
 GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';
 ALTER USER 'root'@'localhost' IDENTIFIED BY '12345';
 FLUSH PRIVILEGES;
 EOF
+    touch "$FLAG_FILE"
+fi
 
-echo "Base de données initialisée avec succès"
-
-exec mariadbd --user=mysql --datadir=/var/lib/mysql
+exec mariadbd --user=mysql --datadir=$DATA_DIR
