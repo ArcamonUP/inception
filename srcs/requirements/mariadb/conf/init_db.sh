00a079b81c645e47f3983c608759344c03d00748
ArcamonUP <kilfen@outlook.fr>
2025-06-19 14:38:37 +0200

    try 3

--- srcs/requirements/mariadb/conf/init_db.sh
+++ srcs/requirements/mariadb/conf/init_db.sh
@@ -2,23 +2,24 @@
 set -e
 
 DATA_DIR=/var/lib/mysql
-FLAG=$DATA_DIR/.initialized
+FLAG_FILE=$DATA_DIR/.initialized
 
 chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
+chmod -R 755 /var/lib/mysql
 
 if [ ! -d "$DATA_DIR/mysql" ]; then
     mariadbd --initialize-insecure --user=mysql --datadir=$DATA_DIR
 fi
 
-if [ ! -f "$FLAG" ]; then
+if [ ! -f "$FLAG_FILE" ]; then
     mariadbd --user=mysql --bootstrap <<EOF
 CREATE DATABASE IF NOT EXISTS $DB_NAME;
 CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
 GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';
-ALTER USER IF EXISTS 'root'@'localhost' IDENTIFIED BY '12345';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '12345';
 FLUSH PRIVILEGES;
 EOF
-    touch "$FLAG"
+    touch $FLAG_FILE
 fi
 
 exec mariadbd --user=mysql --datadir=$DATA_DIR
