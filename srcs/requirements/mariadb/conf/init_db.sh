03e96c0a83dd4011392d8c91a2d56590fe8bbbab
ArcamonUP <kilfen@outlook.fr>
2025-06-19 13:59:55 +0200

    fix: bullseye

--- srcs/requirements/mariadb/conf/init_db.sh
+++ srcs/requirements/mariadb/conf/init_db.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-pkill -f mysqld || true
-rm -f /var/lib/mysql/aria_log_control
-rm -f /var/lib/mysql/ibdata1.lock
-rm -f /var/run/mysqld/mysqld.pid
+set -eo pipefail
 
-chown -R mysql:mysql /var/lib/mysql
-chown -R mysql:mysql /var/run/mysqld
+rm -f /var/lib/mysql/aria_log_control /var/lib/mysql/ibdata1.lock
+rm -f /var/run/mysqld/mysqld.pid || true
+
+chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
 chmod -R 755 /var/lib/mysql
 
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-    mysql_install_db --user=mysql --datadir=/var/lib/mysql
+    mariadbd --initialize-insecure --user=mysql --datadir=/var/lib/mysql
 fi
 
-mysqld --user=mysql --bootstrap --verbose=0 --skip-networking=0 <<EOF
-USE mysql;
+mariadbd --user=mysql --bootstrap <<EOF
 FLUSH PRIVILEGES;
 CREATE DATABASE IF NOT EXISTS $DB_NAME;
 CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
@@ -25,4 +23,4 @@ EOF
 
 echo "Base de données initialisée avec succès"
 
-exec mysqld --user=mysql --datadir=/var/lib/mysql
+exec mariadbd --user=mysql --datadir=/var/lib/mysql
